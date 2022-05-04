255c386f3c5d6dae692f3fef5f8c3b3d59e681f4
HIC P <nicholas.parrish@mac.com>
2022-05-04 13:54:47 -0400

    stopping point

--- src/app/base-cal/base-cal.component.ts
+++ src/app/base-cal/base-cal.component.ts
@@ -60,7 +60,7 @@ export class BaseCalComponent implements OnInit {
 
 
 
-  view: CalendarViewType = CalendarViewType.Month;
+  view: CalendarViewType = CalendarViewType.List;
   CalendarViewType = CalendarViewType;
   viewDate: Date = new Date();
   viewDateString = "Today";
@@ -204,9 +204,9 @@ export class BaseCalComponent implements OnInit {
 
   actions: CalendarEventAction[] = [
     {
-      label: `<h1><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
-    </svg></h1>`,
+      label: `<div><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+    </svg></div>`,
       a11yLabel: 'move',
       onClick: ({ event }: { event: CalendarEvent }): void => {
         console.log(event)
